6f2f7fa2b96774eb35ef26a43f27a5d770a55518
antsafiaiantsoa-a11y <antsafiaiantsoa@gmail.com>
2026-02-03 11:38:57 +0100

    index

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const port = 3000;
 
 app.use(cors({
-  origin: "http://localhost:5174", 
+  origin: "https://reactepetitpaspro.vercel.app", 
   credentials: true
 }));
 app.use(express.json());
